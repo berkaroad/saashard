797aa7d1952c44999cc7c78f9a30dcfb59f14712
Jerry Bai <chinaren_bjr@163.com>
2016-10-11 11:51:52 +0800

    update version

--- version.go
+++ version.go
@@ -1,6 +1,6 @@
 package saashard
 
 const (
-	Version = "2016-10-08 05:47:45 +0800 @ac1235c"
-	Compile = "2016-10-08 06:34:14 +0800 by go version go1.7.1 darwin/amd64"
+	Version = "2016-10-11 11:34:16 +0800 @0987dfa"
+	Compile = "2016-10-11 11:41:14 +0800 by go version go1.7.1 darwin/amd64"
 )
